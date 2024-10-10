8a76cc5713b768a433e99825aa20a785df663e5c
Subhrashis Das <das.subhrashis@gmail.com>
2024-10-10 16:38:09 +0530

    Observality codebase

---
+++ appsmith/alloy.sh
@@ -0,0 +1,6 @@
+apt install gpg
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install alloy
